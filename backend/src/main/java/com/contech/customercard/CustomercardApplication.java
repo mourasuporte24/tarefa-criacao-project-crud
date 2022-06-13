419b08f380f7fcb8f8bdbfbe856ff54ab55537c5
mourasuporte24 <mourasuporte24@gmail.com>
2022-06-12 21:49:00 -0300

    Project Created

---
+++ backend/src/main/java/com/contech/customercard/CustomercardApplication.java
@@ -0,0 +1,13 @@
+package com.contech.customercard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomercardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomercardApplication.class, args);
+	}
+
+}
